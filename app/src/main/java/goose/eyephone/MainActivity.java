493fad6bf752ed12b65352c993e3cfc0c1c067bc
GustavoSilvera <gustavo.r.silvera@gmail.com>
2018-03-14 22:47:21 -0700

    added second activity for camera

--- app/src/main/java/goose/eyephone/MainActivity.java
+++ app/src/main/java/goose/eyephone/MainActivity.java
@@ -1,15 +1,11 @@
 package goose.eyephone;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import android.widget.Button;
@@ -17,15 +13,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-public class MainActivity extends Activity  {
-    Button b1,b2;
-    EditText ed1,ed2;
+public class MainActivity extends AppCompatActivity {
+    Button login,cancel;
+    EditText nameEnter,passwordEnter;
 
     TextView tx1;
     int counter = 9;//total of nine tries
@@ -35,85 +26,45 @@ public class MainActivity extends Activity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b1 = (Button)findViewById(R.id.button);
-        ed1 = (EditText)findViewById(R.id.editText);
-        ed2 = (EditText)findViewById(R.id.editText2);
+        login = (Button)findViewById(R.id.login);
+        nameEnter = (EditText)findViewById(R.id.enterName);
+        passwordEnter = (EditText)findViewById(R.id.passwordText);
 
-        b2 = (Button)findViewById(R.id.button2);
+        cancel = (Button)findViewById(R.id.cancel);
         tx1 = (TextView)findViewById(R.id.textView3);
         tx1.setVisibility(View.GONE);
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            if(ed1.getText().toString().equals("urmom") &&//username
-                    ed2.getText().toString().equals("dotcom")) {//password
-                //do stuff
-                Toast.makeText(getApplicationContext(),"Pivoting...",Toast.LENGTH_SHORT).show();
-                dispatchTakePictureIntent();
-                //setContentView(R.layout.overlay);
-            }
-            else{
-                //do other stuff
-                Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
-                tx1.setVisibility(View.VISIBLE);
-                //tx1.setBackgroundColor(Color.RED);
-                counter--;
-                tx1.setText(Integer.toString(counter));
+                if(nameEnter.getText().toString().equals("Goose") &&//username
+                        passwordEnter.getText().toString().equals("thunderpants")) {//password
+                    //do stuff
+                    Toast.makeText(getApplicationContext(),"Pivoting...",Toast.LENGTH_SHORT).show();
+                    //dispatchTakePictureIntent();
+                    setContentView(R.layout.overlay);
+                    startActivity(new Intent(MainActivity.this,imageOverlay.class));
 
-                if (counter == 0) {
-                    b1.setEnabled(false);
+                }
+                else{
+                    //do other stuff
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+                    tx1.setVisibility(View.VISIBLE);
+                    //tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        login.setEnabled(false);
+                    }
                 }
             }
-            }
         });
-        b2.setOnClickListener(new View.OnClickListener() {
+        cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-    }
-
-    String mCurrentPhotoPath;
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "yaBoiPic";//"JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-    static final int REQUEST_TAKE_PHOTO = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "goose.eyephone.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-
-            }
-        }
     }
 }
--- app/src/main/java/goose/eyephone/imageOverlay.java
+++ app/src/main/java/goose/eyephone/imageOverlay.java
@@ -1,17 +1,97 @@
 package goose.eyephone;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by silvera94198 on 3/7/2018.
  */
 
-public class imageOverlay extends Activity {
+public class imageOverlay extends Activity{
+    Button camera,select,back;
+    String mCurrentPhotoPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.overlay);
+
+        camera = (Button)findViewById(R.id.cameraButton);
+        select = (Button)findViewById(R.id.galleryButton);
+        back = (Button)findViewById(R.id.backButton);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();//runs camera thing
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(imageOverlay.this,MainActivity.class));
+            }
+        });
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(imageOverlay.this,MainActivity.class));
+            }
+        });
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img";//"JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "goose.eyephone.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
+            }
+        }
+    }
 }
