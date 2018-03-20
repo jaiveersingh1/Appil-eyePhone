b63c637756fa77068ba8fbb4723b0aeb42832f99
GustavoSilvera <gustavo.r.silvera@gmail.com>
2018-03-19 20:26:24 -0700

    got gallery adn camera thing working, :dab:

--- app/src/main/java/goose/eyephone/imageOverlay.java
+++ app/src/main/java/goose/eyephone/imageOverlay.java
@@ -2,6 +2,7 @@ package goose.eyephone;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -22,14 +23,15 @@ import java.util.Date;
 /**
  * Created by silvera94198 on 3/7/2018.
  */
-
+//¯\_(ツ)_/¯
 public class imageOverlay extends Activity{
     Button camera,select,back;
     String mCurrentPhotoPath;
     private static final int PICK_IMAGE = 100;
     Uri imageUri;
-    ImageView imageView;
-
+    ImageView eyesOpen;
+    ImageView eyesClosed;
+    boolean isOpenDone = false;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -38,11 +40,15 @@ public class imageOverlay extends Activity{
         camera = (Button)findViewById(R.id.cameraButton);
         select = (Button)findViewById(R.id.galleryButton);
         back = (Button)findViewById(R.id.backButton);
+        eyesOpen = (ImageView)findViewById(R.id.eyesOpen);
+        eyesClosed = (ImageView)findViewById(R.id.eyesClosed);
 
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakePictureIntent();//runs camera thing
+               // dispatchTakePictureIntent();//runs camera thing
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 5327);
             }
         });
         back.setOnClickListener(new View.OnClickListener() {
@@ -66,7 +72,7 @@ public class imageOverlay extends Activity{
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
+                ".jpg",    /* suffix */
                 storageDir      /* directory */
         );
 
@@ -94,7 +100,6 @@ public class imageOverlay extends Activity{
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-
             }
         }
     }
@@ -107,9 +112,28 @@ public class imageOverlay extends Activity{
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
-            imageUri = data.getData();
-            imageView.setImageURI(imageUri);
+        if(!isOpenDone){//first starting with eyes open
+            if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+                imageUri = data.getData();
+                eyesOpen.setImageURI(imageUri);
+            }
+            else if (requestCode == 5327){
+                Bitmap Bitmap = (Bitmap)data.getExtras().get("data");
+                eyesOpen.setImageBitmap(Bitmap);
+            }
+            isOpenDone = true;//toggles the imageView
+        }
+        else {
+            if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+                imageUri = data.getData();
+                eyesClosed.setImageURI(imageUri);
+            }
+            else if (requestCode == 5327){
+                Bitmap Bitmap = (Bitmap)data.getExtras().get("data");
+                eyesClosed.setImageBitmap(Bitmap);
+            }
+            isOpenDone = false;//toggles the imageView
+
         }
     }
 }
